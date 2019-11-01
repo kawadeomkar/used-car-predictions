9732da78effde7411f3b35625952f65cc1f6f632
kawadeomkar <kawade.omkar21@gmail.com>
2019-10-31 18:54:28 -0700

    updated vehicle scraper, removed unnecessary files, created sh file for starting zookeeper/kafka

--- datacollection/cityScraper/lambda_function.py
+++ datacollection/cityScraper/lambda_function.py
@@ -18,24 +18,39 @@ def lambda_handler(event, context):
     
     city_json = {} 
     counter = 0
+    bold = 0
 
     for item in cities:
         name = item.text
        
-        #if name == None, text is in bold
+        # if name == None, text is in bold
         if name == None:
-            name = item.xpath("//b")[boldAt].text
-        
+            name = item.xpath("//b")[bold].text
+            bold += 1
+         
         city_json[counter] = {}
         city_json[counter]["url"] = item.attrib['href']
         city_json[counter]["name"] = name.replace("'", "''")
-        
+        counter += 1 
+    
+    
+    # temporarily dump json to local text file for uploading to s3, fix later
+    with open ('cities.txt', 'w') as file:
+        json.dump(city_json, file)    
+
+
+    """    
     # pass to s3
-    #s3 = boto3.resource('s3')
-    #s3Object = s3.Object('CL_CITIES', 'cities.json')
-    #s3Object.put(Body=(bytes(json.dumps(city_json).encode('UTF-8'))))
+    s3 = boto3.resource('s3')
+    s3Object = s3.Object('CL_CITIES', 'cities.json')
+    s3Object.put(Body=(bytes(json.dumps(city_json).encode('UTF-8'))))
     
     return {
         'statusCode': 200,
         'body': json.dumps('Done crawling for cities')
-    } 
+    }
+    """ 
+
+
+if __name__ == '__main__':
+     lambda_handler(None, None)
